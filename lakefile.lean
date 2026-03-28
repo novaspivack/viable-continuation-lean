7d163bf06266a60183dacb72aec95a883aaeb49a
novaspivack <nova@novaspivack.com>
2026-03-28 19:22:11 -0400

    Unify toolchain to rc6
    Bump from v4.29.0-rc3 to v4.29.0-rc6 to match rest of program.

--- lakefile.lean
+++ lakefile.lean
@@ -5,7 +5,7 @@ package «viable-continuation-lean» where
   -- Viable Continuation Under Constraint: general theory of stability, pathology, collapse.
 
 require mathlib from git
-  "https://github.com/leanprover-community/mathlib4.git" @ "v4.29.0-rc3"
+  "https://github.com/leanprover-community/mathlib4.git" @ "v4.29.0-rc6"
 
 @[default_target]
 lean_lib «ViableContinuation» where
